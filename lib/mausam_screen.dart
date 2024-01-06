f737d582cd5f4fff541834e6c52a2c1ed770f28e
Aditya Dwivedi <dwikaljit@gmail.com>
2024-01-06 19:43:04 +0530

    further simplified main screen's build function

--- lib/mausam_screen.dart
+++ lib/mausam_screen.dart
@@ -9,107 +9,123 @@ class MausamScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Mausam",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-        actions: [
-          InkWell(
-            onTap: () {},
-            child: IconButton(
-              icon: const Icon(Icons.refresh),
-              onPressed: () {},
-            ),
-          ),
-        ],
-      ),
-      body: const Center(
+      appBar: _buildAppBar(),
+      body: Center(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              WeatherCard(
-                temperature: "15 °C",
-                icon: Icons.cloud,
-                weather: "Cloudy",
-              ),
-              SizedBox(height: 20),
-              Text(
+              _buildWeatherCard(),
+              const SizedBox(height: 20),
+              const Text(
                 "Weather Forecast",
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    HourlyWeatherForecastWidget(
-                      time: "09:00",
-                      icon: Icons.cloud,
-                      temperature: "15°C",
-                    ),
-                    HourlyWeatherForecastWidget(
-                      time: "09:00",
-                      icon: Icons.cloud,
-                      temperature: "15°C",
-                    ),
-                    HourlyWeatherForecastWidget(
-                      time: "09:00",
-                      icon: Icons.cloud,
-                      temperature: "15°C",
-                    ),
-                    HourlyWeatherForecastWidget(
-                      time: "09:00",
-                      icon: Icons.cloud,
-                      temperature: "15°C",
-                    ),
-                    HourlyWeatherForecastWidget(
-                      time: "09:00",
-                      icon: Icons.cloud,
-                      temperature: "15°C",
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
+              const SizedBox(height: 10),
+              _buildWeatherForecastWidget(),
+              const SizedBox(height: 20),
+              const Text(
                 "Additional Information",
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  AdditionalInformationWidget(
-                    icon: Icons.water_drop,
-                    text: "Humidity",
-                    value: "94",
-                  ),
-                  AdditionalInformationWidget(
-                    icon: Icons.air,
-                    text: "Wind Speed",
-                    value: "7.67",
-                  ),
-                  AdditionalInformationWidget(
-                    icon: Icons.beach_access,
-                    text: "Pressure",
-                    value: "1006",
-                  ),
-                ],
-              )
+              const SizedBox(height: 10),
+              _buildAdditionalInformationWidget(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Row _buildAdditionalInformationWidget() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        AdditionalInformationWidget(
+          icon: Icons.water_drop,
+          text: "Humidity",
+          value: "94",
+        ),
+        AdditionalInformationWidget(
+          icon: Icons.air,
+          text: "Wind Speed",
+          value: "7.67",
+        ),
+        AdditionalInformationWidget(
+          icon: Icons.beach_access,
+          text: "Pressure",
+          value: "1006",
+        ),
+      ],
+    );
+  }
+
+  SingleChildScrollView _buildWeatherForecastWidget() {
+    return const SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          HourlyWeatherForecastWidget(
+            time: "09:00",
+            icon: Icons.cloud,
+            temperature: "15°C",
+          ),
+          HourlyWeatherForecastWidget(
+            time: "09:00",
+            icon: Icons.cloud,
+            temperature: "15°C",
+          ),
+          HourlyWeatherForecastWidget(
+            time: "09:00",
+            icon: Icons.cloud,
+            temperature: "15°C",
+          ),
+          HourlyWeatherForecastWidget(
+            time: "09:00",
+            icon: Icons.cloud,
+            temperature: "15°C",
+          ),
+          HourlyWeatherForecastWidget(
+            time: "09:00",
+            icon: Icons.cloud,
+            temperature: "15°C",
+          ),
+        ],
+      ),
+    );
+  }
+
+  WeatherCard _buildWeatherCard() {
+    return const WeatherCard(
+      temperature: "15 °C",
+      icon: Icons.cloud,
+      weather: "Cloudy",
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        "Mausam",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+      actions: [
+        InkWell(
+          onTap: () {},
+          child: IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {},
+          ),
+        ),
+      ],
     );
   }
 }
